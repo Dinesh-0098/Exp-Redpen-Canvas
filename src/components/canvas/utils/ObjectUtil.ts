8c980dbd04bc4f65c721bee2206e9161332e0602
Dinesh Patil <dinesh.patil@ajmerainfotech.com>
2020-10-12 15:40:16 +0530

    22-23

--- src/components/canvas/utils/ObjectUtil.ts
+++ src/components/canvas/utils/ObjectUtil.ts
@@ -414,13 +414,16 @@ export type FabricObjects = {
  * @param {{ [key: string]: any }} [properties]
  */
 export const toObject = (obj: any, propertiesToInclude: string[], properties?: { [key: string]: any }) =>
-	fabric.util.object.extend(
-		obj.callSuper('toObject'),
-		propertiesToInclude.reduce(
-			(prev, property) =>
-				Object.assign(prev, {
-					[property]: obj.get(property),
-				}),
-			Object.assign({}, properties),
-		),
-	);
+	{
+		debugger
+		return fabric.util.object.extend(
+			obj.callSuper('toObject'),
+			propertiesToInclude.reduce(
+				(prev, property) =>
+					Object.assign(prev, {
+						[property]: obj.get(property),
+					}),
+				Object.assign({}, properties),
+			),
+		);
+	}
--- webpack.prod.js
+++ webpack.prod.js
@@ -23,37 +23,16 @@ const plugins = [
 ];
 module.exports = merge(baseConfig, {
 	mode: 'production',
-	devtool:'#source-map',
+	devtool:'source-map',
 	entry: {
 		vendor: ['react', 'react-dom', 'lodash', 'fabric', 'antd'],
-		app: ['@babel/polyfill', path.resolve(__dirname, 'src/index.js')],
+		app: ['@babel/polyfill', path.resolve('src/index.js')],
 	},
 	output: {
 		// entry에 존재하는 app.js, vendor.js로 뽑혀 나온다.
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/[name].js',
-		chunkFilename: 'js/[id].[chunkhash:16].js',
 		publicPath: './',
-	},
-	optimization: {
-		minimizer: [
-			// we specify a custom UglifyJsPlugin here to get source maps in production
-			new TerserPlugin({
-				cache: true,
-				parallel: true,
-				terserOptions: {
-					warnings: false,
-					compress: {
-						warnings: false,
-						unused: true, // tree shaking(export된 모듈 중 사용하지 않는 모듈은 포함하지않음)
-					},
-					ecma: 6,
-					mangle: true,
-					unused: true,
-				},
-				sourceMap: true,
-			}),
-		],
 	},
 	plugins,
 });
